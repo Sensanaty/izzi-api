0ffe3f0c308e4ca88e6ba5aa2f0224131b68b5cf
Sensanaty <salevic@lu-ka.me>
2023-01-21 14:21:48 +0100

    Always permit `controller`, `action` and `format` params

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,7 @@ module IzziApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.action_controller.always_permitted_parameters = %w( controller action format )
   end
 end
