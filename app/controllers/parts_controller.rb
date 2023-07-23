6648af64a42620162955cc4b45aa573bd84c565b
Luka <salevic@lu-ka.me>
2023-07-23 16:02:34 +0200

    Handle pagy overflow

--- app/controllers/parts_controller.rb
+++ app/controllers/parts_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class PartsController < ApplicationController
+  rescue_from Pagy::OverflowError, with: :page_overflow
+
   def index
     search = params[:query]
 
@@ -9,9 +11,16 @@ class PartsController < ApplicationController
               .includes(:company)
               .length
 
-    @pagy, @parts = pagy(Part.where('part_number ILIKE ?', "%#{search}%").includes(:company), items: params[:count] || 25)
+    @pagy, @parts = pagy(Part.where('part_number ILIKE ?', "%#{search}%").includes(:company),
+                         items: params[:count] || 25)
     @metadata = pagy_metadata(@pagy)
 
     render 'parts/index', status: :ok
+  end
+
+  private
+
+  def page_overflow
+    render json: { error: 'Page is not in valid range' }, status: :unprocessable_entity
   end
 end
