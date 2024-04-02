b70c0f9d8fba95f2b37122a2ad109fa816c8eaae
Luka <salevic@lu-ka.me>
2024-04-02 17:02:31 +0200

    Add DAP Aero to CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -23,4 +23,12 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
              headers: :any,
              methods: %i[get post put patch delete options head]
   end
+
+  allow do
+    origins(%r{(https://)(.*dap-aero.com)})
+
+    resource '*',
+             headers: :any,
+             methods: %i[get post put patch delete options head]
+  end
 end
