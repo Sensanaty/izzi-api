d401664eb0683bb72f9b04419224cb157de9338f
Luka <salevic@lu-ka.me>
2024-10-22 15:08:06 +0200

    Validate presence of name on Company creation

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Company < ApplicationRecord
+  validates_presence_of :name
+
   has_many :clients, dependent: :destroy
   has_many :parts, dependent: :destroy
 end
