97ac2e6607c7df60d60484e9175f3a916046273d
Luka <salevic@lu-ka.me>
2025-01-06 14:19:38 +0100

    Add preview pages to CORS allowlist

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -31,4 +31,12 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
              headers: :any,
              methods: %i[get post put patch delete options head]
   end
+
+  allow do
+    origins(%r{(https://)(.*dap-aero.pages.dev)})
+
+    resource '*',
+             headers: :any,
+             methods: %i[get post put patch delete options head]
+  end
 end
