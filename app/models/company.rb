65bc5f69ef2e5e27f4ff5d32f142d111155711df
Sensanaty <salevic@lu-ka.me>
2025-05-11 11:51:38 +0200

    Add counter cache to `parts` in Company model

--- app/models/company.rb
+++ app/models/company.rb
@@ -4,5 +4,5 @@ class Company < ApplicationRecord
   validates_presence_of :name
 
   has_many :clients, dependent: :destroy
-  has_many :parts, dependent: :destroy
+  has_many :parts, dependent: :destroy, counter_cache: true
 end
